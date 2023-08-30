af7618d9f7d783262680fff984853f7f70353ec9
Nicholas Mejia <nicknmejia@gmail.com>
2023-08-30 16:17:16 -0700

    Added loader and logic to show/hide it

--- script.js
+++ script.js
@@ -3,26 +3,42 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter-btn');
 const newQuoteBtn = document.getElementById('new-quote-btn');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
 
     // Check if Author field is blank and replace it with 'Unknown'
     authorText.textContent = !quote.author ? 'Unknown' : quote.author;
-    quoteText.textContent = quote.text;
-
     if (quote.text.length > 50) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
     }
+
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
@@ -44,6 +60,7 @@ function tweetQuote() {
 
 // On Load
 getQuotes();
+    
 
 // Event Listeners
 twitterBtn.addEventListener('click', tweetQuote);
