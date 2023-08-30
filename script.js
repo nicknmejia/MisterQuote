efb107d69a1ed4a30b637f0975a82e896b82295d
Nicholas Mejia <nicknmejia@gmail.com>
2023-08-30 16:06:30 -0700

    Implemented button logic for tweeting quotes to local user's twitter account, and generate a new quote on button click

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-btn');
+const newQuoteBtn = document.getElementById('new-quote-btn');
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
-    console.log(quote);
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    authorText.textContent = !quote.author ? 'Unknown' : quote.author;
+    quoteText.textContent = quote.text;
+
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
 }
 
 // Get Quotes from API
@@ -19,5 +34,17 @@ async function getQuotes() {
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+    window.open(twitterUrl, '_blank');
+
+}
+
 // On Load
 getQuotes();
+
+// Event Listeners
+twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener('click', newQuote);
